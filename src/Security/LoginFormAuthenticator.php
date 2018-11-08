30116eb2a33b72c5fe5797f5d08402adbea9c86d
urankajtazaj <ukajtazi@gmail.com>
2018-11-08 13:35:32 +0100

    show post thumbnail on edit form

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -68,7 +68,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            throw new CustomUserMessageAuthenticationException('Username or password is incorrect.');
         }
 
         return $user;
