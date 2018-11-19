4c496ce62a24c249ea5c009aa995cbb55811d281
urankajtazaj <ukajtazi@gmail.com>
2018-11-19 16:09:07 +0100

    added archive for each category

--- src/Controller/ArchiveController.php
+++ src/Controller/ArchiveController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Post;
+use App\Entity\Category;
 
 class ArchiveController extends AbstractController
 {
@@ -32,8 +33,21 @@ class ArchiveController extends AbstractController
      */
     public function post_category($category)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $cat = $em->getRepository(Category::class)->findOneBy(['name' => $category]);
+
+        $posts_qb = $em->getRepository(Post::class)->createQueryBuilder('p')
+            ->where('p.category = :category')
+            ->setParameter('category', $cat)
+            ->orderBy('p.id', 'DESC')
+            ->getQuery();
+
+        $posts = $posts_qb->setMaxResults(10)->execute();
+
         return $this->render('archive/index.html.twig', [
             'controller_name' => 'ArchiveController',
+            'posts' => $posts
         ]);
     }
 
